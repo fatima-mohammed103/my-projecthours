cd22bfe7229e92b031d29b22fd9ac4d374d01109
fatima-mohammed103 <fatimmmm789@gmail.com>
2026-04-26 14:45:07 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+
+/*let but=document.querySelector('shop');
+//but.addEventListener("click",function(){console.log('fatma mohammed')})
+but.onclick=one;
+
+function one(){console.log('fatma mohammed')}
+one()*/
